f2882378b5e9897986fa27327cb313a41731fc10
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:43:03 +0200

    initial commit: added test.js with some simple Math :P

---
+++ test.js
@@ -0,0 +1,19 @@
+var myVariableName = 5;
+
+var a = 5;
+var b = 6;
+
+console.log(a+b);
+
+var a = "5";
+var b = 6;
+
+console.log(a+b);
+
+console.log(typeof(a+b));
+
+
+console.log(Math.sin(1));
+console.log(Math.floor(7.8));
+console.log(Math.ceil(7.4));
+console.log(Math.max(7.4,80,90));
